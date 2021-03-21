4589d82984f0675b35d4046af7babd714853470d
Shamiul Islam <Shamiul@gmail.com>
2021-03-21 15:06:50 +0600

    Role table model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('role_id')->default();
+            $table->string('phone_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->default('avatar.jpg');
+            $table->string('status')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
